71f331f2cf3399de51ea84269fcc61ba5bcb18e9
RM-RITIK <mehtaritik11@gmail.com>
2019-06-01 21:44:24 +0530

    third commit

--- joinUs.php
+++ joinUs.php
@@ -8,14 +8,15 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Team Defianz Racing</title>
+    <title>Team Definz Racing</title>
     <style>
-    .color {
+     .color {
       background: #F8F9FA;
     }
-    
-
-
+    .form {
+        margin-top: 60px;
+    }
+   
     
     </style>
   </head>
@@ -36,10 +37,10 @@
 </nav>
 <ul class="nav nav-tabs color">
   <li class="nav-item">
-    <a class="nav-link active" href="#">About</a>
+    <a class="nav-link" href="index.php">About</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="joinUs.php">Join Us</a>
+    <a class="nav-link active" href="joinUs.php">Join Us</a>
   </li>
   <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">The Crew</a>
@@ -78,54 +79,62 @@
   
 
 </ul>
-<div id="carouselExampleIndicators" class="carousel slide margin" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="im1.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="im2.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="im3.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+<h4  class = "form"><center>TEAM DEFINZ RACING</center></h4>
+<section id="cover" >
+    <div id="cover-caption">
+        <div id="container" class="container">
+            <div class="row text-white">
+                <div class="col-sm-6 offset-sm-3 text-center">
+                    <div class="info-form">
+                        <form action="" class="form-inlin justify-content-center">
+                        
+                            <div class="form-group">
+                                <label class="sr-only">Name</label>
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Email</label>
+                                <input type="email" class="form-control" placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Contact No.</label>
+                                <input type="text" class="form-control" placeholder="Contact No.">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">College</label>
+                                <input type="text" class="form-control" placeholder="College">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Graduation Year</label>
+                                <input type="text" class="form-control" placeholder="Graduation Year">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Address</label>
+                                <input type="text" class="form-control" placeholder="Address">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">City</label>
+                                <input type="text" class="form-control" placeholder="City">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">State</label>
+                                <input type="text" class="form-control" placeholder="State">
+                            </div>
+                            
 
-<div class="card-group">
-  <div class="card">
-    
-    <div class="card-body">
-      <h5 class="card-title">OUR VISION</h5>
-      <p class="card-text">TEAM DEFINZ RACING SEEKS TO INCULCATE LEARNING AND INNOVATION AMONG BUDDING ENGINEERS. TO DO SO THE TEAM STRIVES TOWARD PROFESSIONAL ENGINEERING PRACTICES
-        AND ACTIVE KNOWLEDGE TRANSFER AMONG STUDENT</p>
-        <a href="learnmore.php" class="btn btn-primary">LEARN MORE</a>
-      
-    </div>
-  </div>
-  <div class="card">
-    
-    <div class="card-body">
-      <h5 class="card-title"> OUR MISSON</h5>
-      <p class="card-text">TO BE THE BEST MAN IN INDIA AND TO BE RECOGNIZED GLOBALLY  AS FORCE TO BE RECKONED WITH.</p>
-      
-    </div>
-  </div>
 
-</div>
+                            <button type="submit" class="btn btn-success ">Submit</button>
+                        </form>
+                    </div>
+                    <br>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
