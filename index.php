5272d69a83417d395205215fcbc23cad3c0af9c9
RM-RITIK <mehtaritik11@gmail.com>
2019-06-01 18:17:17 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Team Defianz Racing</title>
+    <style>
+    
+
+
+    
+    </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">TEAM DEFINZ RACING</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+
+    </ul>
+    <span class="navbar-text">
+      DELHI TECHNOLOGICAL UNIVERSITY 
+    </span>
+  </div>
+</nav>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">About</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Join Us</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Full Time</a>
+      <a class="dropdown-item" href="#">Minors</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">The Crew</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">2019</a>
+      <a class="dropdown-item" href="#">2018</a>
+      <a class="dropdown-item" href="#">2017</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">The Cars</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Media</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Blog</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Driverless Car</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">About</a>
+      <a class="dropdown-item" href="#">Joining Driverless</a>
+      <a class="dropdown-item" href="#">Driverless News and Media</a>
+      <a class="dropdown-item" href="#">Driverless Crew</a>
+
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Our Sponsors</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+  
+  
+
+</ul>
+<div id="carouselExampleIndicators" class="carousel slide margin" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="im1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="im2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="im3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="card-group">
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">OUR VISION</h5>
+      <p class="card-text">TEAM DEFINZ RACING SEEKS TO INCULCATE LEARNING AND INNOVATION AMONG BUDDING ENGINEERS. TO DO SO THE TEAM STRIVES TOWARD PROFESSIONAL ENGINEERING PRACTICES
+        AND ACTIVE KNOWLEDGE TRANSFER AMONG STUDENT</p>
+        <a href="#" class="btn btn-primary">LEARN MORE</a>
+      
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title"> OUR MISSON</h5>
+      <p class="card-text">TO BE THE BEST MAN IN INDIA AND TO BE RECOGNIZED GLOBALLY  AS FORCE TO BE RECKONED WITH.</p>
+      
+    </div>
+  </div>
+
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
